91dc0a2bae49ee1e17cc3abca5ceeb125f83ada3
BrotatoTornado <mykoloasmeninis@gmail.com>
2026-09-24 02:05:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
+#include <stdexcept>
 
 #include "muhhash.h"
+
+int failas(const std::string& path)
+{
+    printf("FAILO MAISA\n");
+
+    try
+    {
+        Hasher d = hashingFile(path);
+        printf("%s\n", ToHex(d).c_str());
+        return 0;
+    }
+    catch (const std::exception& e)
+    {
+        fprintf(stderr, "Blogai: nepavyko perskaityti failo. %s\n", e.what());
+        return 1;
+    }
+}
+
 
 int tekstas()
 {
@@ -30,5 +49,30 @@ int tekstas()
 
 int main()
 {
-    return tekstas();
+    while (true)
+    {
+        printf("Ivestis ranka ar failas?\n1 - Ranka\n2 - Failas\n");
+
+        int c = 0;
+        std::cin >> c;
+        std::cin.ignore();
+
+        if (c == 1)
+        {
+            return tekstas();
+        }
+        else if (c == 2)
+        {
+            printf("Failo kelias: ");
+            std::string kelias;
+            std::getline(std::cin, kelias);
+            return failas(kelias);
+        }
+        else
+        {
+            printf("Blogas pasirinkimas, bandyk dar karta.\n\n");
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+        }
+    }
 }
